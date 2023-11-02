17bbaa76d2a6899bba840ab7fdfd4dae75f95d1b
Drljevic, Omar <omar.drljevic@tuwien.ac.at>
2023-11-02 17:06:11 +0100

    i broke it, abudction mask

--- tensor_ergonomics.py
+++ tensor_ergonomics.py
@@ -39,15 +39,14 @@ class RULAXXX():
                            10: 'Trunk_sidebend', 11: 'Neck', 12: 'Neck_sidebend',
                            13: 'Neck_twist'}
 
-        self.score_total = torch.tensor([])
+        self.score_total = torch.tensor([], requires_grad=True, dtype=torch.float32)
 
     def compute_scores(self):
-        # Elementary scores
-        score_upper_arm = torch.tensor([], dtype=torch.float32)
-        score_lower_arm = torch.tensor([], dtype=torch.float32)
-        score_trunk = torch.tensor([], dtype=torch.float32)
-        score_neck = torch.tensor([], dtype=torch.float32)
-        score_legs = torch.tensor([], dtype=torch.float32)
+        score_upper_arm = torch.tensor([], requires_grad=True, dtype=torch.float32)
+        score_lower_arm = torch.tensor([], requires_grad=True, dtype=torch.float32)
+        score_trunk = torch.tensor([], requires_grad=True, dtype=torch.float32)
+        score_neck = torch.tensor([], requires_grad=True, dtype=torch.float32)
+        score_legs = torch.tensor([], requires_grad=True, dtype=torch.float32)
 
         # Global scores
         score_A = []
@@ -55,126 +54,129 @@ class RULAXXX():
         score_total = []
 
         angles = self.accumulate_angles()  # This should return a torch tensor
-        print('Angles:', type(angles))
+        #print('Angles:', type(angles))
 
 
         for i, frame in enumerate(angles):
-            print('--- Frame {}: ---'.format(i))
-            print('Frame:', frame)
+            #print('--- Frame {}: ---'.format(i))
+
             # A. Arms Step 1
             max_shoulder = torch.max(torch.abs(frame[0]), torch.abs(frame[2]))
-            print('Max shoulder:', max_shoulder)
+            #print('Max shoulder:', max_shoulder)
 
-            if (max_shoulder <= 20).item():
-                score_val = torch.tensor(1.0)
-            elif (max_shoulder <= 45).item():
-                score_val = torch.tensor(2.0)
-            elif (max_shoulder <= 90).item():
-                score_val = torch.tensor(3.0)
-            else:
-                score_val = torch.tensor(4.0)
+            score_val = torch.where(max_shoulder <= 20, torch.tensor(1.0, requires_grad=True), torch.tensor(2.0, requires_grad=True))
+            score_val = torch.where((max_shoulder > 20) & (max_shoulder <= 45), torch.tensor(2.0, requires_grad=True), score_val)
+            score_val = torch.where((max_shoulder > 45) & (max_shoulder <= 90), torch.tensor(3.0, requires_grad=True), score_val)
+            score_val = torch.where(max_shoulder > 90, torch.tensor(4.0, requires_grad=True), score_val)
 
             score_upper_arm = torch.cat((score_upper_arm, score_val.unsqueeze(0)))
 
             # A. Check Abduction
             max_abduction = torch.max(frame[1], frame[3])
-            print('Max abduction:', max_abduction)
+            #print('Max abduction:', max_abduction)
 
-            if (max_abduction > 150).item():
-                #score_upper_arm[i] += torch.tensor(1.0)
-                score_upper_arm[i] = score_upper_arm[i] + 1.0
+            # Create a mask for the condition
+            #abduction_mask = (max_abduction > 150).float()  # This will be 1.0 if true, 0.0 if false
+            abduction_mask = (max_abduction > 150).float().unsqueeze(0)
+            score_upper_arm = score_upper_arm + abduction_mask
 
+            # Use the mask to update the score_upper_arm tensor
+            # The unsqueeze(0) is necessary to match the dimensions for broadcasting
+            #score_upper_arm = score_upper_arm + abduction_mask.unsqueeze(0)
+            score_upper_arm = torch.add(score_upper_arm, abduction_mask.unsqueeze(0))
 
             # A. Arms Step 2
             max_elbow = torch.max(frame[4], frame[5])
-            print('Max elbow:', max_elbow)
+            #print('Max elbow:', max_elbow)
 
-            if (max_elbow <= 20).item() or (max_elbow >= 100).item():
-                score_val = torch.tensor(2.0)
-            else:
-                score_val = torch.tensor(1.0)
+            # Use torch.where to handle the conditional logic in a differentiable way
+            score_val = torch.where((max_elbow <= 20) | (max_elbow >= 100),
+                                    torch.tensor(2.0, requires_grad=True),
+                                    torch.tensor(1.0, requires_grad=True))
 
+            # Concatenate the score_val to score_lower_arm
+            # Make sure score_lower_arm is a tensor that will have requires_grad=True if necessary
             score_lower_arm = torch.cat((score_lower_arm, score_val.unsqueeze(0)))
 
-            #index = ((score_upper_arm[i]-1)*3 + (score_lower_arm[i] - 1)).long()
-            index = ((score_upper_arm[i] - torch.tensor(1.0)) * 3 + (score_lower_arm[i] - torch.tensor(1.0))).long()
-
-            print('Score Upper Arm:', score_upper_arm)
-            print('Score Lower Arm:', score_lower_arm)
+            # For indexing, you don't need requires_grad=True because indexing is not a differentiable operation
+            # However, ensure that the result of the indexing (curr_score_A) is used in a way that maintains the graph if necessary
+            index = ((score_upper_arm[i] - 1) * 3 + (score_lower_arm[i] - 1)).long()
             curr_score_A = self.table_A[index]
 
-            print('Current Score A:', curr_score_A)
+            #print('Current Score A:', curr_score_A)
             score_A.append(curr_score_A)
 
-            # B. Neck ???
-            if frame[11] >= 40:
-                score_val = torch.tensor([2], dtype=torch.float32)
-            elif frame[11] >= 20:
-                score_val = torch.tensor([1], dtype=torch.float32)
-            elif frame[11] < 20:
-                score_val = torch.tensor([4], dtype=torch.float32)
-            else:
-                score_val = torch.tensor([3], dtype=torch.float32)
+            # B. Neck
+            score_val = torch.where(frame[11] >= 40, torch.tensor([2.0], requires_grad=True),
+                                    torch.where(frame[11] >= 20, torch.tensor([1.0], requires_grad=True),
+                                                torch.where(frame[11] < 20, torch.tensor([4.0], requires_grad=True),
+                                                            torch.tensor([3.0], requires_grad=True))))
             score_neck = torch.cat((score_neck, score_val))
 
-            # Neck Side Bending
-            #if (frame[12] >= 120).item() or (frame[12] <= 60).item():
-            #    score_neck[-1] += torch.tensor(1.0)
+            # Update score_neck with side bending condition
             condition = (frame[12] >= 120) | (frame[12] <= 60)
-            score_neck[-1] = score_neck[-1] + condition.float()
+            # Ensure that the update does not break the computational graph
+            #score_neck[-1] = score_neck[-1] + condition.float() * torch.tensor(1.0, requires_grad=True)
+            #score_neck = torch.cat((score_neck[:-1], score_neck[-1:] + condition.float() * torch.tensor(1.0, requires_grad=True)))
+            score_neck = torch.cat((score_neck[:-1], score_neck[-1:] + condition.float() * torch.tensor(1.0, requires_grad=True)))
 
-            print('Score Neck:', score_neck)
 
-            # B. Trunk
+            #print('Score Neck:', score_neck)
+
+            # Create score_val with requires_grad=True if necessary
             mask1 = (frame[8] <= 15).float()
             mask2 = ((frame[8] > 15) & (frame[8] <= 30)).float()
             mask3 = ((frame[8] > 30) & (frame[8] <= 60)).float()
             mask4 = (frame[8] > 60).float()
-
-            # Apply masks to possible scores
             score_val = 1.0 * mask1 + 2.0 * mask2 + 3.0 * mask3 + 4.0 * mask4
+            score_val = score_val.unsqueeze(0)  # Add requires_grad=True if this tensor should be part of the gradient computation
+            score_trunk = torch.cat((score_trunk, score_val))
 
-            score_trunk = torch.cat((score_trunk, score_val.unsqueeze(0)))
+            # For score_trunk updates
+            additional_score = ((frame[10] <= 60) | (frame[10] >= 120)).float() * torch.tensor(1.0, requires_grad=True)
+            additional_score += (frame[9] >= 30).float() * torch.tensor(1.0, requires_grad=True)
 
-            """if (frame[10] <= 60).item() or (frame[10] >= 120).item():
-                score_trunk[i] += torch.tensor(1.0)
-            if (frame[9] >= 30).item():
-                score_trunk[i] += torch.tensor(1.0)"""
-            score_trunk[i] += ((frame[10] <= 60) | (frame[10] >= 120)).float()
-            score_trunk[i] += (frame[9] >= 30).float()
-            print('Score Trunk:', score_trunk)
+            # Now we use the additional_score to update score_trunk without in-place operations
+            # Ensure that all tensors involved have requires_grad=True if necessary
+            updated_score_trunk = score_trunk[i] + additional_score
+            score_trunk = torch.cat((score_trunk[:i], updated_score_trunk.unsqueeze(0), score_trunk[i+1:]))
+            #print('Score Trunk:', score_trunk)
 
-            # B. Legs
+            # For score_legs
             min_knee = torch.min(frame[6], frame[7])
-            print('Min knee:', min_knee)
+            #print('Min knee:', min_knee)
 
-            """if (min_knee >= 90).item():
-                score_val = torch.tensor(2.0)
-            else:
-                score_val = torch.tensor(1.0)"""
-            condition = (min_knee >= 90)
-            score_val = torch.where(condition, torch.tensor(2.0), torch.tensor(1.0))
+            # Use torch.where to handle the conditional logic in a differentiable way
+            score_val = torch.where(min_knee >= 90, torch.tensor(2.0, requires_grad=True), torch.tensor(1.0, requires_grad=True))
 
-
+            # Concatenate the score_val to score_legs
             score_legs = torch.cat((score_legs, score_val.unsqueeze(0)))
-            print('Score Legs:', score_legs)
+            #print('Score Legs:', score_legs)
 
+            # For indexing, you don't need requires_grad=True because indexing is not a differentiable operation
+            # However, ensure that the result of the indexing (curr_score_B) is used in a way that maintains the graph if necessary
             index1 = (score_neck[i] - 1).long()
-            index2 = ((score_trunk[i] - 1)*2 + (score_legs[i] - 1)).long()
+            index2 = ((score_trunk[i] - 1) * 2 + (score_legs[i] - 1)).long()
             curr_score_B = self.table_B[index1][index2]
-            print('Current Score B:', curr_score_B)
+            #print('Current Score B:', curr_score_B)
             score_B.append(curr_score_B)
 
+            # Assuming curr_score_A and curr_score_B are tensors that require gradients
+            curr_score_A_clamped = torch.clamp(curr_score_A, max=8.0)
+            curr_score_B_clamped = torch.clamp(curr_score_B, max=7.0)
 
-            curr_score_A = torch.min(curr_score_A, torch.tensor(8.0, requires_grad=True))
-            curr_score_B = torch.min(curr_score_B, torch.tensor(7.0, requires_grad=True))
-
-            index1 = (curr_score_A - 1).long()
-            index2 = (curr_score_B - 1).long()
+            # Indexing to get curr_total
+            index1 = (curr_score_A_clamped - 1).long()
+            index2 = (curr_score_B_clamped - 1).long()
             curr_total = self.table_C[index1][index2]
-            print('Current Total Score:', curr_total)
+            #print('Current Total Score:', curr_total)
 
+            # Append curr_total to score_total
             score_total.append(curr_total)
+
+            # After the loop, convert score_total to a tensor if you need to compute gradients with respect to it
+            self.score_total = torch.stack(score_total)  # This will have requires_grad=True if curr_total has it
+
 
         self.score_total = torch.stack(score_total)
 
@@ -234,7 +236,7 @@ class RULAXXX():
             angle_neck_twist = self.calculate_twist(pose, 11, 14, 9, 10)
             angles_frame.append(angle_neck_twist)
 
-            angles_frame_tensor = torch.tensor(angles_frame)
+            angles_frame_tensor = torch.tensor(angles_frame, requires_grad=True)
             all_angles.append(angles_frame_tensor)
 
         return torch.stack(all_angles, dim=0)
@@ -249,11 +251,15 @@ class RULAXXX():
         new_pose = torch.transpose(pose, 0, 1)
 
         # Swap the second and third dimensions
-        new_pose[1, :], new_pose[2, :] = new_pose[2, :].clone(), new_pose[1, :].clone()
+        #new_pose[1, :], new_pose[2, :] = new_pose[2, :].clone(), new_pose[1, :].clone()
+        temp = new_pose[1, :].clone()
+        new_pose[1, :] = new_pose[2, :].clone()
+        new_pose[2, :] = temp
 
         # Flip the Z axis
-        new_pose[2, :] = -new_pose[2, :]
-
+        #new_pose[2, :] = -new_pose[2, :]
+        new_pose[2, :] = new_pose[2, :].neg()
+    
         return new_pose
 
     def calculate_z(self, pose, joint1, joint2):
@@ -261,14 +267,14 @@ class RULAXXX():
         b = pose[:, joint2]
 
         ba = a - b
-        bc = torch.tensor([0.0, 0.0, 1.0], device=pose.device)
+        bc = torch.tensor([0.0, 0.0, 1.0], requires_grad=True, device=pose.device)
 
         cosine_angle = torch.dot(ba, bc) / (torch.norm(ba) * torch.norm(bc))
         angle = torch.acos(cosine_angle)
 
         angle_degrees = angle * (180 / torch.pi)
 
-        return angle_degrees.item()
+        return angle_degrees
 
     def calculate_angle(self, pose, joint1, joint2, joint3):
         a = pose[:, joint1]
@@ -301,3 +307,7 @@ class RULAXXX():
 
         # 7. Degree Conversion
         return angle * (180.0 / torch.pi)
+
+#%%
+
+#%%
